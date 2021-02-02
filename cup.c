c7804b4f93e8cd5823f2362203d47efefd94e956
Plankiton <pl4nk1ton@gmail.com>
2021-02-02 17:02:40 -0300

    Prepar to include icons on output

--- cup.c
+++ cup.c
@@ -10,6 +10,7 @@ char *f = NULL, *s = NULL;
 char *cmd, *cat, *proj, *subdir;
 char * al [4], * al2 [2], * arglist [6];
 
+const int types [] = {4, -1};
 int main(int argc, char *argv[]) {
     program_name = argv[0];
 
@@ -21,43 +22,58 @@ int main(int argc, char *argv[]) {
     root_path = strcat(root_path, "Create/");
 
     const int types [] = {4, -1};
-    int count = count_items(root_path, types);
-    if (count < 0)
+    int cat_count = count_items(root_path, types);
+    if (cat_count < 0)
         die("\"%s\" %s", root_path, "not exists, create it first");
-    char ** cat_list = ls(root_path, count, types);
+    char ** cat_list = ls(root_path, cat_count, types);
 
     if (!strcmp(cmd, "-L")) {
-        if (count > 0) {
-            for (int i = 0; i < count; i ++) {
-                if (strcmp(cat_list[i], ".")&&strcmp(cat_list[i], ".."))
-                    puts(cat_list[i]);
-            }
+        for (int i = 0; i < cat_count; i ++) {
+            if (strcmp(cat_list[i], ".")&&strcmp(cat_list[i], ".."))
+                puts(cat_list[i]);
         }
-    } else if (!strcmp(cmd, "-l")) {
+    } else if (!strcmp(cmd, "-l") && cat_count > 0) {
         if (f)
             cat = f;
+        if (s)
+            proj = s;
+
         if (cat) {
             char * category = NULL;
-            if (count > 0) {
-                category = get_from_patt(cat, cat_list, count);
-            }
+            category = get_from_patt(cat, cat_list, cat_count);
 
-            if (category)
+            if (proj) {
+                char ** proj_list = get_list_proj(category);
+                int count = get_list_proj_count(category);
+
+                char * project = get_from_patt(proj, proj_list, count);
+
+                if (project) {
+
+                    char proj_path[] = "";
+                    strcat(proj_path, get_cat_path(category));
+                    strcat(proj_path, project);
+
+                    int count = count_items(proj_path, NULL);
+                    char ** proj_files = ls(proj_path, count, NULL);
+                    for (int i = 0; i < count; i ++) {
+                        if (strcmp(proj_files[i], ".")&&strcmp(proj_files[i], ".."))
+                            puts(proj_files[i]);
+                    }
+
+                    free_list(proj_files, count);
+                } else die("Project not found!");
+                free_list(proj_list, count);
+                exit(0);
+            } else if (category)
                 list_proj(category);
             else
                 die("Category not found!");
 
         } else {
-            int lenght = 0;
-            for (int i = 0; i < count; i ++) {
-                int cat_len = strlen(cat_list[i]);
-                if (lenght < cat_len)
-                    lenght = cat_len;
-            }
-
             show_cat = 1;
             putchar('\n');
-            for (int i = 0; i < count; i ++) {
+            for (int i = 0; i < cat_count; i ++) {
                 if (strcmp(cat_list[i], ".")&&strcmp(cat_list[i], ".."))
                 {
                     cat = cat_list[i];
@@ -68,7 +84,7 @@ int main(int argc, char *argv[]) {
                     list_proj(cat);
 
                     putchar('\n');
-                    if (i < count - 2)
+                    if (i < cat_count - 2)
                         putchar('\n');
                 }
             }
@@ -76,7 +92,7 @@ int main(int argc, char *argv[]) {
 
     }
 
-    free_list(cat_list, count);
+    free_list(cat_list, cat_count);
     return 0;
 }
 
@@ -93,20 +109,37 @@ char * get_from_patt(char * patt, char **list, int count) {
     return NULL;
 }
 
-int has_proj(char * category) {
-    const int types [] = {4, -1};
-    char cat_path [strlen(root_path)+strlen(category)];
+char * get_cat_path(char * category) {
+    char * cat_path = malloc(strlen(root_path)+strlen(category));
     strcpy(cat_path, root_path);
 
     strcat(cat_path, category);
     if (cat_path[strlen(cat_path)-1] != '/')
         strcat(cat_path, "/");
+
+    return cat_path;
+}
+
+int has_proj(char * category) {
+    char * cat_path = get_cat_path(category);
 
     int count = count_items(cat_path, types);
     return count>2 ? count : 0;
 }
 
-void list_proj(char * category) {
+int get_list_proj_count(char * category) {
+    const int types [] = {4, -1};
+    char cat_path [strlen(root_path)+strlen(category)];
+    strcpy(cat_path, root_path);
+
+    strcat(cat_path, category);
+    if (cat_path[strlen(cat_path)-1] != '/')
+        strcat(cat_path, "/");
+
+    return count_items(cat_path, types);
+}
+
+char ** get_list_proj(char * category) {
     const int types [] = {4, -1};
     char cat_path [strlen(root_path)+strlen(category)];
     strcpy(cat_path, root_path);
@@ -116,7 +149,12 @@ void list_proj(char * category) {
         strcat(cat_path, "/");
 
     int count = count_items(cat_path, types);
-    char ** proj_list = ls(cat_path, count, types);
+    return ls(cat_path, count, types);
+}
+
+void list_proj(char * category) {
+    char ** proj_list = get_list_proj(category);
+    int count = get_list_proj_count(category);
     for (int i = 0; i < count; i ++) {
         if (strcmp(proj_list[i], ".")&&strcmp(proj_list[i], "..")){
             if (show_cat)
